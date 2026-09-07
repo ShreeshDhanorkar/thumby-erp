ba7bcf6e91fdd4422d64402e9646aad4d94b986e
Shreesh <dhanorkarshreesh@gmail.com>
2026-09-07 08:58:34 +0530

    Update cache name from v8 to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "thumby-erp-v8";
+const CACHE_NAME = "thumby-erp-v9";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./initial-inventory.json"];
 
 self.addEventListener("install", (event) => {
